83ee630d3491857d7a18cc34c138cafb51a326e9
LiuRich <181250087@smail.nju.edu.cn>
2021-03-12 11:32:25 +0800

    3.12.11

---
+++ editor/cn/[200]岛屿数量.java
@@ -0,0 +1,77 @@
+//给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。 
+//
+// 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。 
+//
+// 此外，你可以假设该网格的四条边均被水包围。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：grid = [
+//  ["1","1","1","1","0"],
+//  ["1","1","0","1","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","0","0","0"]
+//]
+//输出：1
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：grid = [
+//  ["1","1","0","0","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","1","0","0"],
+//  ["0","0","0","1","1"]
+//]
+//输出：3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == grid.length 
+// n == grid[i].length 
+// 1 <= m, n <= 300 
+// grid[i][j] 的值为 '0' 或 '1' 
+// 
+// Related Topics 深度优先搜索 广度优先搜索 并查集 
+// 👍 1026 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int numIslands(char[][] grid) {
+        int sum = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    sum++;
+                    dfs(i, j, grid);
+                }
+            }
+        }
+        return sum;
+    }
+
+    public void dfs(int row, int column, char[][] grid) {
+        if (!inAres(row, column, grid)) return;
+        if (grid[row][column] != '1') return;
+        grid[row][column] = '2';
+        dfs(row - 1, column, grid);
+        dfs(row + 1, column, grid);
+        dfs(row, column - 1, grid);
+        dfs(row, column + 1, grid);
+    }
+
+    public boolean inAres(int row, int column, char[][] grid) {
+        if (row >= 0 && row < grid.length && column >= 0 && column < grid[0].length) return true;
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ editor/cn/[695]岛屿的最大面积.java
@@ -0,0 +1,64 @@
+//给定一个包含了一些 0 和 1 的非空二维数组 grid 。 
+//
+// 一个 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在水平或者竖直方向上相邻。你可以假设 grid 的四个边缘都被 
+//0（代表水）包围着。 
+//
+// 找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为 0 。) 
+//
+// 
+//
+// 示例 1: 
+//
+// [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+// [0,0,0,0,0,0,0,1,1,1,0,0,0],
+// [0,1,1,0,1,0,0,0,0,0,0,0,0],
+// [0,1,0,0,1,1,0,0,1,0,1,0,0],
+// [0,1,0,0,1,1,0,0,1,1,1,0,0],
+// [0,0,0,0,0,0,0,0,0,0,1,0,0],
+// [0,0,0,0,0,0,0,1,1,1,0,0,0],
+// [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+// 
+//
+// 对于上面这个给定矩阵应返回 6。注意答案不应该是 11 ，因为岛屿只能包含水平或垂直的四个方向的 1 。 
+//
+// 示例 2: 
+//
+// [[0,0,0,0,0,0,0,0]] 
+//
+// 对于上面这个给定的矩阵, 返回 0。 
+//
+// 
+//
+// 注意: 给定的矩阵grid 的长度和宽度都不超过 50。 
+// Related Topics 深度优先搜索 数组 
+// 👍 448 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int max=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    int temp=dfs(i,j,grid);
+                    if(max<temp)max=temp;
+                }
+            }
+        }
+        return max;
+    }
+
+    public int dfs(int a,int b,int[][] grid){
+        if(!inAres(a,b,grid))return 0;
+        if(grid[a][b]!=1)return 0;
+        grid[a][b]=2;
+        return 1+dfs(a+1,b,grid)+dfs(a-1,b,grid)+dfs(a,b-1,grid)+dfs(a,b+1,grid);
+    }
+
+    public boolean inAres(int a,int b,int[][] grid){
+        if(a>=0&& a< grid.length && b>=0 && b< grid[0].length)return true;
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
